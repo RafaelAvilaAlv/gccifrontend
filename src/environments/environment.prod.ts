ed7ea63cfce63c0b6e0c997823785b05aca1695d
RafaelAvila <milton.avila.est@tecazuay.edu.ec>
2026-05-05 21:02:27 -0500

    ACTUALIZACION

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,4 @@
-//export const environment = {
- // production: true,
- // apiUrl: 'http://localhost:8080'
-//};
-
-
 export const environment = {
   production: true,
-  apiUrl: 'https://gcci-backend.onrender.com'
+  apiUrl: 'http://localhost:8080'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
-//export const environment = {
- // production: false,
- // apiUrl: 'http://localhost:8080'
-//};
-
 export const environment = {
   production: false,
-  apiUrl: 'https://gcci-backend.onrender.com'
+  apiUrl: 'http://localhost:8080'
 };
